0f009308d326d903200f505774009c419350912d
Tiat Han <ooi.tiathan@gmail.com>
2021-04-06 23:55:00 +0800

    Add cors, mongoose and dotenv and test mongodb connection

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
 const app = express()
 const port = 3000
+
+dotenv.config();
+
+app.use(cors())
+app.use(express.json())
+
+const uri = process.env.MONGODB_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, 
+ useUnifiedTopology: true})
+
+const connection = mongoose.connection
+
+connection.once('open', () => {
+  console.log("connect to db success")
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
